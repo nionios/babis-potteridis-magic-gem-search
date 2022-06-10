6acd2dc3a7eef3a2c732c6f3e273890e77adebd7
nionios <dennis.nik@protonmail.com>
2022-06-10 20:28:17 +0300

    Improve intro function

--- include/intro/intro.hpp
+++ include/intro/intro.hpp
@@ -1,1 +1,3 @@
+#include <string>
+void print(std::string str, int offset_x, int offset_y);
 void intro (void);
--- src/intro/intro.cpp
+++ src/intro/intro.cpp
@@ -2,74 +2,41 @@
 #include <unistd.h>
 #include <string>
 // Intro sequence for starting the game
+void print(std::string str, int offset_x, int offset_y) {
+    for (auto character : str) {
+        mvaddch((LINES/2) + offset_y, (COLS/2) + offset_x, character);
+        usleep(50000);
+        refresh();
+        offset_x++;
+    }
+}
+
 void intro (void) {
-    int i = -7;
-    std::string str = "Babis Potteridis";
-    for (auto character : str) {
-        mvaddch((LINES/2) - 4, (COLS/2) + i, character);
-        usleep(50000);
-        refresh();
-        i++;
-    }
-    i = -3;
-    str = "and the";
-    for (auto character : str) {
-        mvaddch((LINES/2) - 3, (COLS/2) + i, character);
-        usleep(50000);
-        refresh();
-        i++;
-    }
-    i = -11;
-    str = "Search of the Magic Gem";
-    for (auto character : str) {
-        mvaddch((LINES/2) - 2, (COLS/2) + i, character);
-        usleep(50000);
-        refresh();
-        i++;
-    }
-    i = -3;
-    str = "*******";
-    for (auto character : str ) {
-        mvaddch((LINES/2), (COLS/2) + i, character);
-        usleep(25000);
-        refresh();
-        i++;
-    }
-    i = -4;
-    str = "*********";
-    for (auto character : str) {
-        mvaddch((LINES/2) + 1, (COLS/2) + i, character);
-        usleep(25000);
-        refresh();
-        i++;
-    }
-    i = -3;
-    str = "*******";
-    for (auto character : str) {
-        mvaddch((LINES/2) + 2, (COLS/2) + i, character);
-        usleep(25000);
-        refresh();
-        i++;
-    }
-    i = -2;
-    str = "*****";
-    for (auto character : str) {
-        mvaddch((LINES/2) + 3, (COLS/2) + i, character);
-        usleep(25000);
-        refresh();
-        i++;
-    }
-    i = -1;
-    str = "***";
-    for (auto character : str) {
-        mvaddch((LINES/2) + 4, (COLS/2) + i, character);
-        usleep(25000);
-        refresh();
-        i++;
-    }
-    i = 0;
-    mvaddch((LINES/2) + 5, (COLS/2) + i, '*');
-    usleep(25000);
-    refresh();
-    i++;
+    int offset_x = -7;
+    int offset_y = -4;
+    print("Babis Potteridis", offset_x, offset_y);
+    offset_x = -3;
+    offset_y++;
+    print("and the", offset_x ,offset_y);
+    offset_x = -11;
+    offset_y++;
+    print("Search of the Magic Gem", offset_x, offset_y);
+    offset_x = -3;
+    offset_y += 2;
+    print("*******", offset_x, offset_y);
+    offset_x--;
+    offset_y++;
+    print("*********", offset_x, offset_y);
+    offset_x++;
+    offset_y++;
+    print("*******", offset_x, offset_y);
+    offset_x++;
+    offset_y++;
+    print("*****", offset_x, offset_y);
+    offset_x++;
+    offset_y++;
+    print("***", offset_x, offset_y);
+    offset_x++;
+    offset_y++;
+    print("*", offset_x, offset_y);
 }
