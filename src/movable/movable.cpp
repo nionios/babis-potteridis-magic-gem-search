8316fdd38e71226cdc3e75eade6eeb7a97d85957
nionios <dennis.nik@protonmail.com>
2022-06-14 12:25:46 +0300

    Remove movable.cpp

--- src/movable/movable.cpp
+++
@@ -1,9 +0,0 @@
-#include <movable.hpp>
-
-void Movable::move_in_y_axis(int block_number){
-    set_y(get_y() - block_number);
-}
-
-void Movable::move_in_x_axis(int block_number){
-    set_x(get_x() - block_number);
-}
